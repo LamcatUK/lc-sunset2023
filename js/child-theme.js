8d22085390545da3c878f712a226c832169c21a2
CheechRockwizard <git@lamcat.co.uk>
2023-11-05 11:23:09 +0000

    nav js diddling

--- js/child-theme.js
+++ js/child-theme.js
@@ -6745,41 +6745,38 @@
 
 	// Add your custom JS here.
 
-	// AOS.init({
-	//   duration: 1000, // values from 0 to 3000, with step 50ms
-	//   once: true,
-	// });
-
 	document.addEventListener('DOMContentLoaded', function () {
 	  const navLinks = document.querySelectorAll('.nav-link');
 	  const navbarCollapse = document.querySelector('.navbar-collapse');
+	  const navbar = document.getElementById('wrapper-navbar');
 	  navLinks.forEach(link => {
 	    link.addEventListener('click', function () {
 	      if (navbarCollapse.classList.contains('show')) {
 	        // Check if the menu is open and close it
 	        navbarCollapse.classList.remove('show');
+	        navbar.classList.add('hidden');
 	      }
 	    });
 	  });
-	});
 
-	// hide beyond height of navbar (--h-top)
-	let lastScrollPosition = 0;
-	const navbar = document.getElementById('wrapper-navbar');
-	const navbarHeight = navbar.clientHeight; // Get the height of the navbar
+	  // hide beyond height of navbar (--h-top)
+	  let lastScrollPosition = 0;
+	  // const navbar = document.getElementById('wrapper-navbar');
+	  const navbarHeight = navbar.clientHeight; // Get the height of the navbar
 
-	window.addEventListener('scroll', function () {
-	  const currentScroll = window.scrollY || document.documentElement.scrollTop;
-	  if (currentScroll > navbarHeight) {
-	    if (currentScroll > lastScrollPosition) {
-	      // Down scroll
-	      navbar.classList.add('hidden');
-	    } else {
-	      // Up scroll
-	      navbar.classList.remove('hidden');
+	  window.addEventListener('scroll', function () {
+	    const currentScroll = window.scrollY || document.documentElement.scrollTop;
+	    if (currentScroll > navbarHeight) {
+	      if (currentScroll > lastScrollPosition) {
+	        // Down scroll
+	        navbar.classList.add('hidden');
+	      } else {
+	        // Up scroll
+	        navbar.classList.remove('hidden');
+	      }
 	    }
-	  }
-	  lastScrollPosition = currentScroll <= 0 ? 0 : currentScroll;
+	    lastScrollPosition = currentScroll <= 0 ? 0 : currentScroll;
+	  });
 	});
 
 	// hide on scroll, assuming --h-top of zero
@@ -6798,6 +6795,11 @@
 	//     }
 
 	//     lastScrollPosition = currentScroll <= 0 ? 0 : currentScroll;
+	// });
+
+	// AOS.init({
+	//   duration: 1000, // values from 0 to 3000, with step 50ms
+	//   once: true,
 	// });
 
 	exports.Alert = alert;
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,45 +1,43 @@
 // Add your custom JS here.
 
-// AOS.init({
-//   duration: 1000, // values from 0 to 3000, with step 50ms
-//   once: true,
-// });
 
 document.addEventListener('DOMContentLoaded', function() {
   const navLinks = document.querySelectorAll('.nav-link');
   const navbarCollapse = document.querySelector('.navbar-collapse');
-
+  const navbar = document.getElementById('wrapper-navbar');
   navLinks.forEach(link => {
       link.addEventListener('click', function() {
           if (navbarCollapse.classList.contains('show')) {
               // Check if the menu is open and close it
               navbarCollapse.classList.remove('show');
+              navbar.classList.add('hidden');
           }
       });
   });
+
+  // hide beyond height of navbar (--h-top)
+  let lastScrollPosition = 0;
+  // const navbar = document.getElementById('wrapper-navbar');
+  const navbarHeight = navbar.clientHeight; // Get the height of the navbar
+  
+  window.addEventListener('scroll', function() {
+      const currentScroll = window.scrollY || document.documentElement.scrollTop;
+  
+      if (currentScroll > navbarHeight) {
+          if (currentScroll > lastScrollPosition) {
+              // Down scroll
+              navbar.classList.add('hidden');
+          } else {
+              // Up scroll
+              navbar.classList.remove('hidden');
+          }
+      }
+  
+      lastScrollPosition = currentScroll <= 0 ? 0 : currentScroll;
+  });
+
 });
 
-
-// hide beyond height of navbar (--h-top)
-let lastScrollPosition = 0;
-const navbar = document.getElementById('wrapper-navbar');
-const navbarHeight = navbar.clientHeight; // Get the height of the navbar
-
-window.addEventListener('scroll', function() {
-    const currentScroll = window.scrollY || document.documentElement.scrollTop;
-
-    if (currentScroll > navbarHeight) {
-        if (currentScroll > lastScrollPosition) {
-            // Down scroll
-            navbar.classList.add('hidden');
-        } else {
-            // Up scroll
-            navbar.classList.remove('hidden');
-        }
-    }
-
-    lastScrollPosition = currentScroll <= 0 ? 0 : currentScroll;
-});
 
 // hide on scroll, assuming --h-top of zero
 // let lastScrollPosition = 0;
@@ -58,3 +56,14 @@ window.addEventListener('scroll', function() {
 
 //     lastScrollPosition = currentScroll <= 0 ? 0 : currentScroll;
 // });
+
+
+
+// AOS.init({
+//   duration: 1000, // values from 0 to 3000, with step 50ms
+//   once: true,
+// });
+
+
+
+
